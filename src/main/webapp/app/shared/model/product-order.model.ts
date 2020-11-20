7b8806e7aa142cbefb9f9468e1669d85d9e553e2
Juanestban <53704567+Juanestban@users.noreply.github.com>
2020-11-19 21:57:25 -0500

    Initial version of store generated by JHipster-6.10.4

---
+++ src/main/webapp/app/shared/model/product-order.model.ts
@@ -0,0 +1,15 @@
+import { Moment } from 'moment';
+import { IOrderItem } from 'app/shared/model/order-item.model';
+import { ICustomer } from 'app/shared/model/customer.model';
+import { OrderStatus } from 'app/shared/model/enumerations/order-status.model';
+
+export interface IProductOrder {
+  id?: number;
+  placedDate?: string;
+  status?: OrderStatus;
+  code?: string;
+  orderItems?: IOrderItem[];
+  customer?: ICustomer;
+}
+
+export const defaultValue: Readonly<IProductOrder> = {};
